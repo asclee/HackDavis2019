afcfc45c7818b89f8762af0d9589ed4c85d5be13
Amber Lee <amblee2009@gmail.com>
2020-09-16 23:47:33 -0700

    Change folder name

--- iOSWorkshop-HackDavis/Are You Mansplaining?/Pods/Target Support Files/Pods-Are You Mansplaining?/Pods-Are You Mansplaining?-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Are_You_Mansplaining_ : NSObject
-@end
-@implementation PodsDummy_Pods_Are_You_Mansplaining_
-@end
---
+++ iOSWorkshop-HackDavis/FallDetector/Pods/Target Support Files/Pods-FallDetector/Pods-FallDetector-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FallDetector : NSObject
+@end
+@implementation PodsDummy_Pods_FallDetector
+@end
